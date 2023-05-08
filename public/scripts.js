173d5f5dc0d1e099de69c6a6962b363778ad17ed
Madison Lester <madiali@email.unc.edu>
2023-05-08 18:02:40 -0400

    Updates

--- public/scripts.js
+++ public/scripts.js
@@ -2,3 +2,18 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+const rpsTitle = document.getElementById("rpsTitle");
+const rpslsTitle = document.getElementById("rpslsTitle");
+const rpsSelection = document.getElementById("rpsSelection");
+const rpslsSelection = document.getElementById("rpslsSelection");
+
+function toggleRpsls() {
+    rpsTitle.classList.toggle("active");
+    rpsTitle.classList.toggle("inactie");
+    rpslsTitle.classList.toggle("active");
+    rpslsTitle.classList.toggle("inactive");
+    rpsSelection.classList.toggle("active");
+    rpsSelection.classList.toggle("inactive");
+    rpslsSelection.classList.toggle("active");
+    rpslsSelection.classList.toggle("inactive");
+}

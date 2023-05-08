3d4a7072891ad479e42c4720b47ac300c3de2323
Madison Lester <madiali@email.unc.edu>
2023-05-08 11:30:14 -0400

    Add rpsls.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,4 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
